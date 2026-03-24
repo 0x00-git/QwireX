4a6d2c84f91c8721ac2e639fe3718981c2b3067f
0x00-git <sasha.goltsev777@gmail.com>
2026-03-24 18:20:47 +0300

    added a header for the code

--- src/qwirex.c
+++ src/qwirex.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2026 0x00 <https://github.com/0x00-git>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/list.h>
